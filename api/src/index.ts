23b00f6d2a385cc06ff384072b3fd737f5c774d1
Adrian Ciura <aciura@gmail.com>
2019-07-16 23:31:31 +0200

    Api version 0.5

--- api/src/index.ts
+++ api/src/index.ts
@@ -8,9 +8,47 @@ const { PORT = 3000 } = process.env
 // app.use(cors(), bodyParser.json())
 
 app.get('/', (req: Request, res: Response) => {
+  // console.log('Request', req)
   res.send({
-    message: 'hello world',
+    message: 'hello from cash-machine',
   })
+})
+
+const availableNotes = [100, 50, 20, 10]
+
+class NoteUnavailableException extends Error {
+  constructor(message?: string) {
+    super(message)
+
+    Object.setPrototypeOf(this, new.target.prototype) // restore prototype chain
+    this.name = NoteUnavailableException.name // stack traces display correctly now
+  }
+}
+
+app.get('/withdraw/:cashAmount', (req, resp) => {
+  console.log('Request /withdraw/:cashAmount', req.params.cashAmount)
+
+  const cashAmount = req.params.cashAmount
+
+  const notesArray = {}
+
+  const [notes, rest] = availableNotes.reduce(
+    ([notesResult, cashLeft], note) => {
+      const notesUsed: number = Math.floor(cashLeft / note)
+      const newCashLeft = cashLeft - notesUsed * note
+      return [{ ...notesResult, [note]: notesUsed }, newCashLeft]
+    },
+    [notesArray, cashAmount]
+  )
+
+  if (rest > 0) {
+    // throw new NoteUnavailableException(
+    //   `Cannot withdraw ${cashAmount}, rest ${rest}`
+    // )
+    resp.status(400).send({ error: 'NoteUnavailableException' })
+  } else {
+    resp.send(notes)
+  }
 })
 
 if (require.main === module) {
